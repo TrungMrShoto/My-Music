80d2f4b65e9d0875abd58d85aae04b05115fb242
TrungMrShoto <trung.nguyen.shotokun@gmail.com>
2019-12-25 20:59:25 +0700

    Step 2: FINISH 75 percents of this app

--- app/src/main/java/com/trongtrung/mymusic/MainActivity.java
+++ app/src/main/java/com/trongtrung/mymusic/MainActivity.java
@@ -4,36 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     TextView txtName, txtInfo, curTime, maxTime;
     SeekBar seekBar;
-    ImageButton btnPlay, btnPrevious, btnNext, btnStop;
-
+    ImageButton btnPlay, btnPrevious, btnNext, btnRepeat;
+    ImageView disc;
     ArrayList<Song> listOfSongs;
     MediaPlayer media;
     int position = 0;
+    SimpleDateFormat formatTime;
+    Animation animation;
+    byte repeatButtonState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Assign();
+        formatTime = new SimpleDateFormat("mm:ss");
+        assignVariables();
         addSong();
-
-        media = MediaPlayer.create(MainActivity.this, listOfSongs.get(position).getFile());
-        txtName.setText(listOfSongs.get(position).getName());
-        txtInfo.setText(listOfSongs.get(position).getSinger());
-
+        initializeMediaPlayer();
+        animation = AnimationUtils.loadAnimation(this,R.anim.rotate_dics);
+        disc.startAnimation(animation);
+        //event for Play Button
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 if (media.isPlaying())
                 {
                     btnPlay.setImageResource(R.drawable.play);
@@ -41,19 +50,84 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else
                 {
-
                     btnPlay.setImageResource(R.drawable.pause);
                     media.start();
                 }
+                setMaxTime();
+                updateTimeSong();
 
             }
         });
 
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeNextSong();
+            }
+        });
+
+        btnPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (media.getCurrentPosition() <= 3000)
+                {                    position--;
+                    if (position < 0) position = listOfSongs.size() - 1;
+                    changeSong();
+                }
+                else {
+                   changeSong();
+                }
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                media.seekTo(seekBar.getProgress());
+            }
+        });
+
+
+        btnRepeat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (repeatButtonState){
+                    case 0:
+                        btnRepeat.setImageResource(R.drawable.repeat_1);
+                        repeatButtonState++;
+                        break;
+                    case 1:
+                        btnRepeat.setImageResource(R.drawable.repeat_2);
+                        repeatButtonState++;
+                        break;
+                    default:
+                        btnRepeat.setImageResource(R.drawable.repeat_0);
+                        repeatButtonState=0;
+                        break;
+                }
+
+            }
+        });
     }
 
-
-
-    private void Assign()
+    private void changeNextSong()
+    {
+        position++;
+        if (position >= listOfSongs.size()) position = 0;
+        changeSong();
+    }
+    private void assignVariables()
     {
         txtName = findViewById(R.id.SongName);
         txtInfo = findViewById(R.id.SongSinger);
@@ -63,13 +137,17 @@ public class MainActivity extends AppCompatActivity {
         btnPlay = findViewById(R.id.ButtonPlay);
         btnNext = findViewById(R.id.ButtonNext);
         btnPrevious = findViewById(R.id.ButtonPrevious);
-        btnStop = findViewById(R.id.ButtonStop);
+        btnRepeat = findViewById(R.id.ButtonRepeat);
+        disc = findViewById(R.id.Disc);
+        repeatButtonState = 0;
+
 
     }
 
+    //add song to list
     private void addSong() {
         listOfSongs = new ArrayList<>();
-
+        listOfSongs.add(new Song("All Is Found", "Evan Rachel Wood", R.raw.all_is_found));
         listOfSongs.add(new Song("Alone", "Alan Walker", R.raw.alone));
         listOfSongs.add(new Song("Faded", "Alan Walker", R.raw.faded));
         listOfSongs.add(new Song("Firework", "Katy Perry", R.raw.firework));
@@ -79,4 +157,72 @@ public class MainActivity extends AppCompatActivity {
         listOfSongs.add(new Song("Señorita", "Shawn Mendes, Camila Cabello", R.raw.senorita));
         listOfSongs.add(new Song("Something Just Like This", "The Chainsmokers, Coldplay", R.raw.something_just_like_this));
     }
+
+    private void initializeMediaPlayer()
+    {
+        media = MediaPlayer.create(MainActivity.this, listOfSongs.get(position).getFile());
+        txtName.setText(listOfSongs.get(position).getName());
+        txtInfo.setText(listOfSongs.get(position).getSinger());
+        setMaxTime();
+        updateTimeSong();
+    }
+
+    private void changeSong()
+    {
+        if (media.isPlaying())
+        {
+            media.stop();
+            media.release();
+        }
+        initializeMediaPlayer();
+        media.start();
+        btnPlay.setImageResource(R.drawable.pause);
+
+    }
+
+    private void setMaxTime()
+    {
+
+        maxTime.setText(formatTime.format(media.getDuration()));
+
+        seekBar.setMax(media.getDuration());
+    }
+
+
+    private void updateTimeSong()
+    {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                curTime.setText(formatTime.format(media.getCurrentPosition()));
+                seekBar.setProgress(media.getCurrentPosition());
+                media.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        if (repeatButtonState == 0)
+                        {
+                            media.stop();
+                            media.release();
+                            initializeMediaPlayer();
+                            btnPlay.setImageResource(R.drawable.play);
+                        }
+                        else if (repeatButtonState == 1)
+                        {
+                            changeNextSong();
+                        }
+                        else
+                        {
+                            position--;
+                            changeNextSong();
+                        }
+
+                    }
+                });
+                handler.postDelayed(this,500);
+            }
+        },100);
+    }
+
+
 }
